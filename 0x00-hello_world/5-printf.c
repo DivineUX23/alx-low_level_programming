c1e0f67127b87ac8ce77cc74e71fe3ec3074c0e2
Divine Igbinoba <96517814+DivineUX23@users.noreply.github.com>
2022-09-09 03:36:12 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,9 @@
 * main - print the string in the put function
 *
 * Description: using the main function
+*
 * this program prints "Programming is like building a multilingual puzzle"
+* 
 * Return: 0
 */
 
