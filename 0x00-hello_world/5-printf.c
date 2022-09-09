9c78d392c996161fb03140701674ae47d4625e89
Divine Igbinoba <96517814+DivineUX23@users.noreply.github.com>
2022-09-09 03:23:38 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
 * main - print the string in the put function
 *
@@ -6,8 +7,10 @@
 * this program prints "Programming is like building a multilingual puzzle
 * Return: 0
 **/
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 }
