90333274c22b6af7afbb91c10bc441fc04b66b0b
Divine Igbinoba <96517814+DivineUX23@users.noreply.github.com>
2022-09-09 03:32:54 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
-
 #include <unistd.h>
-
 
 /**
  * main - print quote
@@ -11,14 +9,9 @@
  * Return: Always 1 (Success)
  */
 
-
-
 int main(void)
-
 {
-
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
-
 }
