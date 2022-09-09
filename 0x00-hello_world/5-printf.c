81ad342aa1efdda3e31a6c028aaa686390618a58
Divine Igbinoba <96517814+DivineUX23@users.noreply.github.com>
2022-09-09 02:56:58 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+
+
 /**
-* Description: main - Prints out a string to stdout.
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
 * Return: 0
-**/
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
