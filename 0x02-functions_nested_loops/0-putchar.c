7a2b4a36cf3b7199b9bee3fdd257537bef851a79
Divine Igbinoba <96517814+DivineUX23@users.noreply.github.com>
2022-09-13 15:25:31 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,10 @@
 #include "main.h"
 
 /**
- * main - prints Putchar
- *
- * Description: prints _putchar
- *
- * Return: 0 (Success)
- */
-
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
 	_putchar('_');
