ab57029dec77e104fcb739964c5b56a52e275f42
Divine Igbinoba <96517814+DivineUX23@users.noreply.github.com>
2022-09-19 12:06:25 +0100

    Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integers
+ * @a: integer to swap
+ * @b: integer to swap
+ * return 0
+ */
+
+int _strlen(char *s);
+{
+  int count = 0;
+
+	while (*(s + count) != '\0')
+		count++;
+  
+	return (count);
+}
